10d3ac7aa44dece56fc836216c868015400c0b27
Jokinhas24 <joaonogueira.art@gmail.com>
2024-05-31 13:36:05 +0100

    Redo the Polindrome Checker

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -4,11 +4,26 @@ namespace Palindrome
 {
     class Program
     {
-        static bool checkIfPalindrome(string word)
+        static bool CheckIfPalindrome(string word)
         {
+            bool isPalindrome()
+            {
+                word = word.ToLower();
+                char[] chars = word.ToCharArray();
+
+                string newString = "";
+
+                foreach (char c in chars)
+                {
+                    newString = $"{c}{newString}";
+                }
+
+                if (newString == word) return true;
+                else return false;
+            }
             if (word == null)
             {
-                throw ArgumentNullException;
+                throw new ArgumentNullException("Empty String.");
             }
             else if (word.Length < 2)
             {
@@ -16,14 +31,15 @@ namespace Palindrome
             }
             else
             {
-                (checkIfPalindrome(word));
+                return isPalindrome();
             }
         }
+        
         static void Main(string[] args)
         {
             foreach (string arg in args)
             {
-                Console.WriteLine($"{arg} -> {checkIfPalindrome(arg)}");
+                Console.WriteLine($"{arg} -> {CheckIfPalindrome(arg)}");
             }
         }
     }
